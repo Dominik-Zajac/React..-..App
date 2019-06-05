e8af83cd1263215de8be06e459bafe97acded9fc
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-05 15:26:13 +0200

    Edit Header (change component on class component)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,49 @@
 import React from 'react';
 
-const Header = () => {
-    return (
-        <div className='row header'>
-            <center><h1>Header</h1></center>
-        </div>
-    )
+class Header extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            bookName: 'Black Books',
+            clicked: true,
+            backgroundColor: 'black',
+            textColor: 'white'
+        }
+    }
+
+    handleClick = () => {
+        console.log('dziala')
+        if (this.state.clicked) {
+
+            this.setState({
+                bookName: 'White Books',
+                clicked: !this.state.clicked,
+                backgroundColor: 'white',
+                textColor: 'black'
+            })
+        } else {
+            this.setState({
+                bookName: 'Black Books',
+                clicked: !this.state.clicked,
+                backgroundColor: 'black',
+                textColor: 'white'
+            })
+        }
+    }
+
+    render() {
+        let headerStyles = {
+            color: this.state.textColor,
+            backgroundColor: this.state.backgroundColor
+        }
+        return (
+            <div className='row header' onClick={this.handleClick} style={headerStyles}>
+                <center><h1>{this.state.bookName}</h1></center>
+            </div>
+        )
+
+    }
 }
 
 export default Header; 
