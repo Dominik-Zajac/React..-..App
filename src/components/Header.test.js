9e38e0f17dc0db73be3e6662f7cb0570f61f8f1d
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-05 15:38:41 +0200

    Edit header.test

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -15,8 +15,19 @@ describe('Header tests', () => {
         ReactDOM.unmountComponentAtNode(div);
     })
 
-    it('Header renders', () => {
+    it('Header changes text according to state', () => {
         const wrapper = shallow(<Header />);
-        expect(wrapper.find('h1').text()).toBe('Header');
+        expect(wrapper.find('h1').text()).toBe('Black Books');
+        wrapper.setState({
+            bookName: 'White Books'
+        })
+        expect(wrapper.find('h1').text()).toBe('White Books');
+    })
+
+    it('Header state changes after clicking on header div', () => {
+        const wrapper = shallow(<Header />);
+        expect(wrapper.state().bookName).toBe('Black Books');
+        wrapper.find('.header').simulate('click');
+        expect(wrapper.state().bookName).toBe('White Books');
     })
 })
