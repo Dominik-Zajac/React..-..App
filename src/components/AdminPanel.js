73c3eccd0d5f207bfb541a26ef6999312e923b91
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-06 16:25:38 +0200

    Add handleChange in AdminPanel

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,29 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AdminPanel = () => {
-    return (
-        <div className='adminPanel col-xs-4'>
-            <form>
-                <div className='form-group'>
-                    <input type='text' placeholder='Book name' id='name' className='form-control' />
-                </div>
-                <div className='form-group'>
-                    <input type='text' placeholder='Book author' id='author' className='form-control' />
-                </div>
-                <div className='form group'>
-                    <textarea placeholder='Book description' id='description' className='form-control'></textarea>
-                </div>
-                <div className='form-group'>
-                    <input type='checkbox' id='onStock' className='form-check-input' name='onStock' />
-                    <label htmlFor='onStock' className='form-check-label'>On stock</label>
-                </div>
-                <div className='form-group'>
-                    <input type='text' placeholder='Book image' id='image' name='image' className='form-control' />
-                </div>
-                <button type='submit' className='btn btn-primary'>Add</button>
-            </form>
-        </div>
-    )
+class AdminPanel extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            name: '',
+            author: '',
+            description: '',
+            onStock: true,
+            image: '',
+        }
+    }
+
+    handleChange = e => {
+        const target = e.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render() {
+        return (
+            <div className='adminPanel col-xs-4'>
+                <form>
+                    <div className='form-group'>
+                        <input
+                            id='name'
+                            name='name'
+                            type='text'
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                            placeholder='Book name'
+                            className='form-control'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            id='author'
+                            name='author'
+                            type='text'
+                            value={this.state.author}
+                            onChange={this.handleChange}
+                            placeholder='Book author'
+                            className='form-control'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <textarea
+                            id='description'
+                            name='description'
+                            value={this.state.description}
+                            onChange={this.handleChange}
+                            placeholder='Book description'
+                            className='form-control'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            id='onStock'
+                            name='onStock'
+                            type='checkbox'
+                            checked={this.state.onStock}
+                            onChange={this.handleChange}
+                            className='form-check-input'
+                        />
+                        <label
+                            htmlFor='onStock'
+                            className='form-check-label'>
+                            On stock
+                        </label>
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            id='image'
+                            name='image'
+                            type='text'
+                            value={this.state.image}
+                            onChange={this.handleChange}
+                            placeholder='Book image'
+                            className='form-control'
+                        />
+                    </div>
+                    <button type='submit' className='btn btn-primary'>Add</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default AdminPanel;
