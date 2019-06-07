02167c74e92f10a62618709d4db739d39256f097
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-07 08:49:07 +0200

    Add OrderView.test

---
+++ src/components/OrderView.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import OrderView from './OrderView';
+import { shallow } from 'enzyme';
+
+describe('Order view tests', () => {
+
+    it('Snapshot matches', () => {
+        const book = {
+            name: 'Book title'
+        }
+        const wrapper = shallow(<OrderView book={book} />);
+        expect(wrapper).toMatchSnapshot();
+    })
+
+})
