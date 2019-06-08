7b6790cfe54cbb35b9355a5c99c7e2a4ad0ac5b7
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-08 12:27:18 +0200

    Update BookView (add description)

--- src/components/BookView.js
+++ src/components/BookView.js
@@ -3,13 +3,19 @@ import React from 'react';
 const BookView = props => {
     return (
         <div className='book-view row'>
-            <div className='col-xs-3'>
-                <h5>Name:</h5>
-                <p>{props.book.name}</p>
+            <div className='col-xs-3 row'>
+                <div className='col-xs-6'>
+                    <h5>Name:</h5>
+                    <p>{props.book.name}</p>
+                </div>
+                <div className='col-xs-6'>
+                    <h5>Author:</h5>
+                    <p>{props.book.author}</p>
+                </div>
             </div>
-            <div className='col-xs-3'>
-                <h5>Author:</h5>
-                <p>{props.book.author}</p>
+            <div className='col-xs-6'>
+                <h5>Description:</h5>
+                <p>{props.book.description}</p>
             </div>
             <div className='col-xs-3'>
                 <button onClick={() => props.addToOrder(props.book)} className='btn btn-primary'>Add to order</button>
