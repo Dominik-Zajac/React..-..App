c103378d831baa53dee0ad5e0eb70af7b5c1d917
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-08 12:04:40 +0200

    Add new component AddBookForm

---
+++ src/components/AddBookForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const AddBookForm = props => {
+    return (
+        <div className='adminPanel col-md-4'>
+            <form onSubmit={props.addNewBook}>
+                <div className='form-group'>
+                    <input
+                        id='name'
+                        name='name'
+                        type='text'
+                        value={props.book.name}
+                        onChange={props.handleChange}
+                        placeholder='Book name'
+                        className='form-control'
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        id='author'
+                        name='author'
+                        type='text'
+                        value={props.book.author}
+                        onChange={props.handleChange}
+                        placeholder='Book author'
+                        className='form-control'
+                    />
+                </div>
+                <div className='form-group'>
+                    <textarea
+                        id='description'
+                        name='description'
+                        value={props.book.description}
+                        onChange={props.handleChange}
+                        placeholder='Book description'
+                        className='form-control'
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        id='onStock'
+                        name='onStock'
+                        type='checkbox'
+                        checked={props.book.onStock}
+                        onChange={props.handleChange}
+                        className='form-check-input'
+                    />
+                    <label
+                        htmlFor='onStock'
+                        className='form-check-label'>
+                        On stock
+                    </label>
+                </div>
+                <button type='submit' className='btn btn-primary'>Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddBookForm;
--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { fbase, firebaseApp } from '../firebase';
 import LoginPanel from './LoginPanel';
+import AddBookForm from './AddBookForm';
 
 class AdminPanel extends Component {
     constructor() {
@@ -88,10 +89,6 @@ class AdminPanel extends Component {
             .catch(() => {
                 alert('Blędy login/hasło!')
             })
-
-        this.setState({
-            loggedIn: true
-        })
     }
 
     componentWillUnmount() {
@@ -110,60 +107,12 @@ class AdminPanel extends Component {
                     />
                 }
                 {this.state.loggedIn &&
-                             <div className='adminPanel col-md-4'>
-                            <form onSubmit={this.addNewBook}>
-                                <div className='form-group'>
-                                    <input
-                                        id='name'
-                                        name='name'
-                                        type='text'
-                                        value={this.state.book.name}
-                                        onChange={this.handleChange}
-                                        placeholder='Book name'
-                                        className='form-control'
-                                    />
-                                </div>
-                                <div className='form-group'>
-                                    <input
-                                        id='author'
-                                        name='author'
-                                        type='text'
-                                        value={this.state.book.author}
-                                        onChange={this.handleChange}
-                                        placeholder='Book author'
-                                        className='form-control'
-                                    />
-                                </div>
-                                <div className='form-group'>
-                                    <textarea
-                                        id='description'
-                                        name='description'
-                                        value={this.state.book.description}
-                                        onChange={this.handleChange}
-                                        placeholder='Book description'
-                                        className='form-control'
-                                    />
-                                </div>
-                                <div className='form-group'>
-                                    <input
-                                        id='onStock'
-                                        name='onStock'
-                                        type='checkbox'
-                                        checked={this.state.book.onStock}
-                                        onChange={this.handleChange}
-                                        className='form-check-input'
-                                    />
-                                    <label
-                                        htmlFor='onStock'
-                                        className='form-check-label'>
-                                        On stock
-                        </label>
-                                </div>
-
-                                <button type='submit' className='btn btn-primary'>Add</button>
-                            </form>
-                        </div>
-                    }
+                    <AddBookForm
+                        book={this.state.book}
+                        addNewBook={this.addNewBook}
+                        handleChange={this.handleChange}
+                    />
+                }
             </div>
         )
     }
