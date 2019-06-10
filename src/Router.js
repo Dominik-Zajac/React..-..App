131c924ba2f41b5ed60d7552ecc0c4f38df1b93a
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-10 13:25:49 +0200

    Install react-redux and add store

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import AdminPanel from './components/AdminPanel';
 import PageNotFound from './components/PageNotFound';
+import store from './store/store';
 
 class Router extends React.Component {
 
     render() {
         return (
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path='/' component={App} />
-                    <Route path='/admin' component={AdminPanel} />
-                    <Route component={PageNotFound} />
-                </Switch>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path='/' component={App} />
+                        <Route path='/admin' component={AdminPanel} />
+                        <Route component={PageNotFound} />
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
         )
     }
 }
